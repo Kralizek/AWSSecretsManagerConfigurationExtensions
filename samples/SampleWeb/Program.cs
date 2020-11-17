bd79fb6759ca755edf3f1c47dbcf2a403480db00
Fabio Di Peri <fabio@kipters.dev>
2020-11-17 21:55:41 +0100

    Update samples to .NET Core 3.1

--- samples/SampleWeb/Program.cs
+++ samples/SampleWeb/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SampleWeb
 {
@@ -14,19 +13,21 @@ namespace SampleWeb
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>  
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    config.AddSecretsManager(configurator: o =>
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder.AddSecretsManager(configurator: o =>
                     {
                         o.PollingInterval = TimeSpan.FromSeconds(10);
                     });
-                })
-                .UseStartup<Startup>()
-                .Build();
+                });
     }
 }
