fd9c4f5d70d751d12e07dc48f8acafc55000a286
devklick <devklick@gmail.com>
2021-09-15 22:30:36 +0100

    Added sample application to demonstrate usage of ReadFromConfigSection

---
+++ samples/Sample7/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Sample7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+
+            /* 
+             * Add some configuration source. 
+             * In this case, we're adding our appsettings JSON file.
+             */
+            builder.AddJsonFile("appsettings.json");
+
+            /*
+             * Add the secrets manager
+             */
+            builder.AddSecretsManager(configurator: op =>
+            {
+                /*
+                 * Configure the secrets manager to read options 
+                 * from a certain config section
+                 */
+                op.ReadFromConfigSection("MySecretsManagerConfig");
+
+                /*
+                 * Can still use the other options here
+                 */
+                op.KeyGenerator = (entry, key) => key.ToUpper();
+            });
+        }
+    }
+}
