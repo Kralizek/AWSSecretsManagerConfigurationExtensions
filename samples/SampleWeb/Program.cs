dd736592f8a57c8376cf442ebb4dcac069892478
Renato Golia <renato.golia@insightarchitectures.com>
2022-11-08 12:09:32 +0100

    Refreshed SampleWeb

--- samples/SampleWeb/Program.cs
+++ samples/SampleWeb/Program.cs
@@ -1,33 +1,9 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace SampleWeb
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+builder.Configuration.AddSecretsManager(configurator: options => options.PollingInterval = TimeSpan.FromSeconds(10));
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureAppConfiguration((context, builder) =>
-                {
-                    builder.AddSecretsManager(configurator: o =>
-                    {
-                        o.PollingInterval = TimeSpan.FromSeconds(10);
-                    });
-                });
-    }
-}
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
