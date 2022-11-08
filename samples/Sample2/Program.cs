3371a848dd5f84567b0a2496c65a6e6504d0540d
Renato Golia <renato.golia@insightarchitectures.com>
2022-11-08 12:02:23 +0100

    Refreshed Sample2

--- samples/Sample2/Program.cs
+++ samples/Sample2/Program.cs
@@ -2,24 +2,15 @@
 using Amazon;
 using Microsoft.Extensions.Configuration;
 
-namespace Sample2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var builder = new ConfigurationBuilder();
+var builder = new ConfigurationBuilder();
 
-            /*
-                Uses default credentials
-                Uses eu-west-1 region
-                Uses default options
-            */
-            builder.AddSecretsManager(region: RegionEndpoint.EUWest1);
+/*
+    Uses default credentials
+    Uses eu-west-1 region
+    Uses default options
+*/
+builder.AddSecretsManager(region: RegionEndpoint.EUWest1);
 
-            var configuration = builder.Build();
+var configuration = builder.Build();
 
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Hello World!");
