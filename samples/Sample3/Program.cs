85c50b58bed702a8cf504f75d92754faef274e6d
Renato Golia <renato.golia@insightarchitectures.com>
2022-11-08 12:02:28 +0100

    Refreshed Sample3

--- samples/Sample3/Program.cs
+++ samples/Sample3/Program.cs
@@ -1,31 +1,21 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 
-namespace Sample3
+var builder = new ConfigurationBuilder();
+
+/*
+    Uses credentials obtained from store
+    Uses default region (from stored profile)
+    Uses default options
+*/
+var chain = new Amazon.Runtime.CredentialManagement.CredentialProfileStoreChain();
+
+if (chain.TryGetProfile("MyProfile", out var profile))
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var builder = new ConfigurationBuilder();
-
-            /*
-                Uses credentials obtained from store
-                Uses default region (from stored profile)
-                Uses default options
-            */
-
-            var chain = new Amazon.Runtime.CredentialManagement.CredentialProfileStoreChain();
-
-            if (chain.TryGetProfile("MyProfile", out var profile))
-            {
-                var credentials = profile.GetAWSCredentials(profile.CredentialProfileStore);
-                builder.AddSecretsManager(credentials, profile.Region);
-            }
-            
-            var configuration = builder.Build();
-
-            Console.WriteLine("Hello World!");
-        }
-    }
+    var credentials = profile.GetAWSCredentials(profile.CredentialProfileStore);
+    builder.AddSecretsManager(credentials, profile.Region);
 }
+            
+var configuration = builder.Build();
+
+Console.WriteLine("Hello World!");
