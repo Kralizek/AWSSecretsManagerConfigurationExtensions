9aa89ed48cd0029d0b873910d2088a6aee1af023
Renato Golia <renato.golia@insightarchitectures.com>
2022-11-08 12:02:58 +0100

    Refreshed Sample6

--- samples/Sample6/Program.cs
+++ samples/Sample6/Program.cs
@@ -3,30 +3,21 @@ using Amazon;
 using Amazon.SecretsManager;
 using Microsoft.Extensions.Configuration;
 
-namespace Sample5
+var builder = new ConfigurationBuilder();
+
+/*
+    Uses the specified client factory method
+*/
+builder.AddSecretsManager(configurator: options =>
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var builder = new ConfigurationBuilder();
+    options.CreateClient = CreateClient;
+});
 
-            /*
-                Uses the specified client factory method
-            */
-            builder.AddSecretsManager(configurator: options =>
-            {
-                options.CreateClient = CreateClient;
-            });
+var configuration = builder.Build();
 
-            var configuration = builder.Build();
+Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Hello World!");
-        }
-
-        private static IAmazonSecretsManager CreateClient()
-        {
-            return new AmazonSecretsManagerClient(RegionEndpoint.EUWest1);
-        }
-    }
+static IAmazonSecretsManager CreateClient()
+{
+    return new AmazonSecretsManagerClient(RegionEndpoint.EUWest1);
 }
